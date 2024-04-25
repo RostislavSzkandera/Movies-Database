88be162b51a9ad9eaeb185d9fdf352d0368c8d51
Rostislav Szkandera <szkandera.r@seznam.cz>
2024-04-25 15:21:59 +0200

    Fix footer height

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className=' fixed  w-full text-center bottom-[0px] flex flex-row justify-center items-center h-8 bg-gray-700 text-white'>
+    <div className=' fixed  w-full text-center bottom-[0px] flex flex-row justify-center items-center h-12 pb-4 bg-gray-700 text-white'>
       <p>&copy; Rostislav Szkandera 2024</p>
     </div>
   )
