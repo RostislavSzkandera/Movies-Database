96f7816ce0dbd1f6504d90de63cc62a7c1567ad2
Rostislav Szkandera <szkandera.r@seznam.cz>
2024-04-29 14:23:44 +0200

    New security firebaseConfig

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,14 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC48W3KZKVqxxLNSvVNQvFsOcxFHKwany8",
-  authDomain: "moviesdatabase-bec96.firebaseapp.com",
-  projectId: "moviesdatabase-bec96",
-  storageBucket: "moviesdatabase-bec96.appspot.com",
-  messagingSenderId: "235595577187",
-  appId: "1:235595577187:web:563a0941e17c6817cbc556"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
 };
+
 
 
 
